48df73c68d9f5f49432967c429b4e70f5b07a9dd
Andrea Quarta <andrea.quarta@porini.it>
2025-01-29 17:39:00 +0100

    news start

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import requests
+# from config import NEWS_API_KEY
+
+
+
+#Create a flask app
+app = Flask(__name__)
+
+
+# Homepage - Route 8fa5a4dda4cf41549f736ebc451202b6
+# Home / About / Contact / Pricing
+
+
+@app.route("/")
+def index():
+    query = request.args.get("query","latest")
+    url = f"https://newsapi.org/v2/everything?q={query}&language=it&apiKey=8fa5a4dda4cf41549f736ebc451202b6"
+    response = requests.get(url)
+    news_data = response.json()
+
+    articles = news_data.get('articles', [])
+        
+    return render_template("index.html", articles=articles)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
